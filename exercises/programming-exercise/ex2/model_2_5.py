666089933e367baa2fc1d43d0a0223b2b974a15a
Arsen Nuramatov <anuramat@pm.me>
2024-12-20 12:34:07 +0100

    feat(ex2): 2.5 (kinda) done; move data

--- exercises/programming-exercise/ex2/model_2_5.py
+++ exercises/programming-exercise/ex2/model_2_5.py
@@ -1,3 +1,8 @@
 # Author: Stefan Haller <stefan.haller@iwr.uni-heidelberg.de>
 
 from tsukuba import *
+import exercise_2 as student
+
+for nodes, edges in all_models():
+    prog, (orig_vars,) = student.convert_to_lp(nodes, edges)
+    prog.solve()
